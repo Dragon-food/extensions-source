b4a219a6d0bfe93c15b355609fe9094a86f2a31b
Dragon-food <dhdbfbfhfbf@gmail.com>
2025-12-29 23:33:53 +0200

    Prochan.kt

--- src/ar/Prochan.kt
+++ src/ar/Prochan.kt
@@ -1,12 +1,9 @@
 package eu.kanade.tachiyomi.extension.ar.prochan
 
 import eu.kanade.tachiyomi.multisource.madara.Madara
-import java.text.SimpleDateFormat
-import java.util.Locale
 
 class Prochan : Madara(
     "Prochan",
     "https://prochan.net",
-    "ar",
-    SimpleDateFormat("MMMM d, yyyy", Locale("ar"))
+    "ar"
 )
